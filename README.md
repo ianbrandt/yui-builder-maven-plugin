b430d1c67a8643da24dade04e69870f8b9490248
Ian Brandt <ian@ianbrandt.com>
2012-07-27 11:51:45 -0700

    Initial project structure.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-yui-maven-plugin
-================
+YUI Builder Maven Plugin
+========================
 
-An M2E compatible Maven plugin for executing incremental YUI component builds.
+An [M2E compatible](http://wiki.eclipse.org/M2E_compatible_maven_plugins) Maven plugin for executing incremental [YUI Build Tool](http://yuilibrary.com/projects/builder/) component builds.
---
+++ src/main/java/com/ianbrandt/tools/maven/yui/builder/BuildMojo.java
@@ -0,0 +1,47 @@
+package com.ianbrandt.tools.maven.yui.builder;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * Goal which executes a YUI Build Tool component build.
+ *
+ * @goal build
+ *
+ * @phase compile
+ */
+public class BuildMojo extends AbstractMojo {
+
+	/**
+	 * Location of the file.
+	 *
+	 * @parameter expression="${project.build.directory}/generated-sources/yui"
+	 * @required
+	 */
+	private File outputDirectory;
+
+	public void execute() throws MojoExecutionException {
+
+		if (!outputDirectory.exists()) {
+			outputDirectory.mkdirs();
+		}
+	}
+}
